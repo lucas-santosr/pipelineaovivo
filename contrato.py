5ca95687a94fcbca044ef05ef8a7547f89d5e065
lucas-santosr <lsr1304@hotmail.com>
2025-01-06 13:59:30 -0300

    Adiciona contrato.py, database.py, docs/ e mkdocs.yml

--- contrato.py
+++ contrato.py
@@ -11,6 +11,19 @@ class ProdutoEnum(str, Enum):
     
 
 class Vendas (BaseModel):
+    """
+    Modelo de dados para as vendas
+
+    Args:
+        email (EmailStr): email do comprador
+        data (datetime): data da compra
+        valor (PositiveFloat): valor da compra
+        produto (PositiveInt): nome do produto
+        quantidade (PositiveInt): quantidade de produtos
+        produto (ProdutoEnum): categoria do produto
+    
+    """
+
     email: EmailStr
     data: datetime
     valor: PositiveFloat
---
+++ docs/index.md
@@ -0,0 +1,5 @@
+# Documentação do Sistema - Estrutura, Funcionalidades e Manutenção
+
+::: contrato.Vendas
+
+::: database.salvar_no_postgres
